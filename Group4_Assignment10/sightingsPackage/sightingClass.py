f1c677cfdb4948a0352cb266e6ac63a079108d5b
hamiltsw <hamiltsw@mail.uc.edu>
2023-11-08 08:55:17 -0500

    second commit

--- Group4_Assignment10/sightingsPackage/sightingClass.py
+++ Group4_Assignment10/sightingsPackage/sightingClass.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 from utilsPackage.utils import distance
 
 '''
@@ -15,12 +15,36 @@ class Sighting():
         self.distance = round(distance(lindLat, lindLng, sighting['lat'], sighting['lng']), 1) # distance from sighting to Lindner College of Business
         self.howMany = sighting.get('howMany', 1)                                              # how many birds were sighted
         try:                                                                                   # date and time of the sighting
-            self.obsDt = datetime.datetime.strptime(sighting['obsDt'], '%Y-%m-%d %H:%M')
+            self.obsDt = datetime.strptime(sighting['obsDt'], '%Y-%m-%d %H:%M')
         except ValueError:
-            self.obsDt = datetime.datetime.strptime(sighting['obsDt'][:10], '%Y-%m-%d')        # if time is not included, only the date is set
+            self.obsDt = datetime.strptime(sighting['obsDt'][:10], '%Y-%m-%d')        # if time is not included, only the date is set
         
     def __str__(self):
-        plural = {True: 's were', False: ' was'}[self.howMany > 1] # adjusts the text for whether multiple birds were sighted or not
-        date = self.obsDt.strftime('%m/%d/%Y')
-        time = self.obsDt.strftime('%H:%M')
-        return f"{self.howMany} {self.comName}{plural} spotted {self.distance} miles away on {date} at {time}!"
+        # adjusts the text for whether there were multiple birds sighted or not
+        plural = {True: 's were', False: ' was'}[self.howMany > 1]
+        
+        # adjusts the text for whether the sighting was minutes, hours, or days ago
+        difference = datetime.now() - self.obsDt
+        if difference.days >= 1:
+            time = f'{difference.days} days'
+        elif difference.seconds // 3600 >= 2:
+            time = f'{difference.seconds // 3600} hours'
+        else:
+            time = f'{difference.seconds // 60} minutes'
+        
+        return f"{self.howMany} {self.comName}{plural} spotted {self.distance} miles away {time} ago!"    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
